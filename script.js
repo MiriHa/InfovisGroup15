0420157adf1431a48f65710a8cb449fee90af099
Don Kanaille <60796711+don-kanaille@users.noreply.github.com>
2020-12-06 13:56:43 +0100

    Set title + rearrange position

--- script.js
+++ script.js
@@ -14,10 +14,12 @@ function visualiseDiagram () {
     var year = [2015, 2016, 2017, 2018, 2019, 2020];
     var data = [0, 10, 50, 60, 70, 20, 80, 100];
 
-    let margin = {top: 10, right: 30, bottom: 30, left:60},
+    // Margin + geometry
+    let margin = {top: 50, right: 30, bottom: 30, left:60},
         width = 660 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom;
 
+    // Container
     var svg = d3.select("#diagrammContainer")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -25,31 +27,37 @@ function visualiseDiagram () {
         .append("g") // Group to container
         .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
+    // Title of Graph
+    svg.append("text")
+        .attr("x", width / 2 )
+        .attr("y", -(margin.top / 2))
+        .style("font", "20px times")
+        .text("Title of Graph");
 
-    var xScale = d3.scaleLinear()
+    // Init xAxis
+    let xScale = d3.scaleLinear()
         //.domain([d3.min(year), d3.max(year)])
         .range([0, width]);
 
+    // Use Generator to customize tickLabels of Axis
     let xAxisGenerator = d3.axisBottom(xScale)
         .tickFormat((d,i) => tickLabels[i])
         .ticks(6);
 
-    let xAxis = svg.append("g")
+    // Set attributes & show xAxis
+    svg.append("g")
         .attr("transform", `translate(0, ${height})`)
         .call(xAxisGenerator)
         .selectAll("text")
         .style("text-anchor", "end")
         .attr("transform", "rotate(-45)" );;
 
-
-    var yScale = d3.scaleLinear()
+    // Init yAxis
+    let yScale = d3.scaleLinear()
         .domain([d3.min(data), d3.max(data)])
         .range([height, 0]);
-/*
-    svg.append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(xScale));*/
 
+    // Show yAxis
     svg.append("g")
         .call(d3.axisLeft(yScale));
 
