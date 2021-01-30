b442fca9a136c1eab3732b43d8db8298a83727ba
Don Kanaille <60796711+don-kanaille@users.noreply.github.com>
2021-01-30 14:16:05 +0100

    TryOut

--- lineDiagram.js
+++ lineDiagram.js
@@ -764,8 +764,103 @@ function visualizeLineDiagram(analogData="", digitalData="", analogSource="", di
             .attr("fill", "none")
             .attr("stroke", color)
             .attr("stroke-width", 2.5)
-            
     }
+
+    var color
+
+    var mouseG = chart.append("g")
+        .attr("class", "mouse-over-effects");
+
+    mouseG.append("path") // this is the black vertical line to follow mouse
+        .attr("class", "mouse-line")
+        .style("stroke", "black")
+        .style("stroke-width", "1px")
+        .style("opacity", "0");
+
+    var lines = document.getElementsByClassName('line');
+
+    var mousePerLine = mouseG.selectAll('.mouse-per-line')
+        .data([analogData, digitalData], function(d){ return d.ser2 })
+        .enter()
+        .append("g")
+        .attr("class", "mouse-per-line");
+
+    mousePerLine.append("circle")
+        .attr("r", 7)
+        .style("stroke", "red")
+        .style("fill", "none")
+        .style("stroke-width", "1px")
+        .style("opacity", "0");
+
+    mousePerLine.append("text")
+        .attr("transform", "translate(10,3)");
+
+    mouseG.append('chart:rect') // append a rect to catch mouse movements on canvas
+        .attr('width', width) // can't catch mouse events on a g element
+        .attr('height', height)
+        .attr('fill', 'none')
+        .attr('pointer-events', 'all')
+        .on('mouseout', function() { // on mouse out hide line, circles and text
+            d3.select(".mouse-line")
+                .style("opacity", "0");
+            d3.selectAll(".mouse-per-line circle")
+                .style("opacity", "0");
+            d3.selectAll(".mouse-per-line text")
+                .style("opacity", "0");
+        })
+        .on('mouseover', function() { // on mouse in show line, circles and text
+            d3.select(".mouse-line")
+                .style("opacity", "1");
+            d3.selectAll(".mouse-per-line circle")
+                .style("opacity", "1");
+            d3.selectAll(".mouse-per-line text")
+                .style("opacity", "1");
+        })
+        .on('mousemove', function() { // mouse moving over canvas
+            //var mouse = d3.pointer(this);
+            var mouse = d3.pointer(event,chart.node());
+            console.log(mouse)
+            console.log("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
+            d3.select(".mouse-line")
+                .attr("d", function() {
+                    var d = "M" + mouse[0] + "," + height;
+                    d += " " + mouse[0] + "," + 0;
+                    return d;
+                });
+
+            d3.selectAll(".mouse-per-line")
+                .attr("transform", function(d, i) {
+                    console.log(width/mouse[0])
+                    var xDate = mouse[0]//.event.pageX//x.invert(mouse[0]),
+                    console.log("++++++++++++++++++++++++++++++++++++++++++")
+                    console.log(xDate)
+                    console.log(mouse)
+                        bisect = d3.bisector(function(d) { return d.ser1; }).right;
+                    idx = bisect(d.ser1, xDate);
+
+                    var beginning = 0,
+                        end = lines[i].getTotalLength(),
+                        target = null;
+
+                    while (true){
+                        target = Math.floor((beginning + end) / 2);
+                        pos = lines[i].getPointAtLength(target);
+                        if ((target === end || target === beginning) && pos.x !== mouse[0]) {
+                            break;
+                        }
+                        if (pos.x > mouse[0])      end = target;
+                        else if (pos.x < mouse[0]) beginning = target;
+                        else break; //position found
+                    }
+
+                    d3.select(this).select('text')
+                        .text(y.invert(pos.y).toFixed(2));
+
+                    return "translate(" + mouse[0] + "," + pos.y +")";
+                });
+        });
+
+
 }
 //Show the sum chart after loading the page for the first time
 //visualizeLineDiagram();
